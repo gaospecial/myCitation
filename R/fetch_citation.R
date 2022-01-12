7be00c5670a9b3527c6088ccad859fc32b3f7588
Chun-Hui Gao <gaospecial@gmail.com>
2022-01-12 23:12:21 +0800

    build cites url

--- R/fetch_citation.R
+++ R/fetch_citation.R
@@ -1,12 +1,5 @@
 # fetch citation of publication(s)
 
-scholar_cites_url = function(cid, total = 0){
-  if (total == 0) return("")
-  if (total <= 10) return(build_scholar_cites_url(cid))
-  if (total > 10) starts = seq(0, total, by = 10)
-  urls = sapply(starts, build_scholar_cites_url, cid = cid)
-  return(urls)
-}
 
 #' build Google Scholar cites URL
 #'
@@ -17,11 +10,18 @@ scholar_cites_url = function(cid, total = 0){
 #' @export
 #'
 #' @examples
-build_scholar_cites_url = function(cid, start = 0){
-  site <- getOption("scholar_site")
-  url_template <- paste0(site, "/scholar?hl=en&start=%d&cites=%s")
-  url <- sprintf(url_template, start, cid)
-  return(url)
+build_scholar_cites_url = function(cid, start = 0, total = 0){
+  if (total == 0) return(NA)
+  if (total <= 10){
+    site <- getOption("scholar_site")
+    url_template <- paste0(site, "/scholar?hl=en&start=%d&cites=%s")
+    url <- sprintf(url_template, start, cid)
+    return(url)
+  } else {
+    starts = seq(0, total, by = 10)
+    sapply(starts, build_scholar_cites_url, cid = cid, total = 10)
+  }
+
 }
 
 #' parse Google Scholar cite page source
@@ -40,6 +40,7 @@ parse_scholar_cites = function(pageSource){
   journal_and_year = line2 %>% stringr::str_remove(".*?-") %>%
     stringr::str_remove("-.*") %>% trimws(whitespace = "[\\h\\v]")
   journal = journal_and_year %>% stringr::str_remove("[0-9]{4}") %>%
+    stringr::str_replace_all("[\\h\\v]"," ") %>%
     trimws(whitespace = "[\\h\\v]") %>%
     stringr::str_remove(",$")
   year = journal_and_year %>% stringr::str_extract("[0-9]{4}")
@@ -83,11 +84,40 @@ query_zotero_translation_server = function(fulltextUrl){
   return(json_data)
 }
 
+#' Parse Zotero JSON data
+#'
+#' @param json a JSON input or a list of JSON input
+#'
+#' @return
+#' @export
+#'
+#' @examples
 parse_zotero_json = function(json){
   if (length(json) == 1){
-    title = json[[1]]$title
-    journal = json[[1]]$publicationTitle
-    year = json[[1]]$date
+    required_col = c("title","publicationTitle","date","creators")
+    present_col = names(json[[1]])
+    l = vector("list", length(required_col))
+    names(l) = required_col
+    for (i in seq_along(l)){
+      col = required_col[[i]]
+      if (col %in% present_col){
+        if (col == "creators"){
+          l[[i]] = sapply(json[[1]][[col]], function(x){
+            paste(x[["firstName"]], x[["lastName"]])
+          }) %>% paste0(collapse = ", ")
+        } else {
+          l[[i]] = json[[1]][[col]]
+        }
+      } else {
+        l[[i]] = NA
+      }
+    }
+    ret = data.frame(l) %>% as_tibble()
+    return(ret)
+  } else {
+    lapply(json, parse_zotero_json) %>%
+      dplyr::bind_rows() %>%
+      unique()
   }
 
 }
